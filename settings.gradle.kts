24ea4b682147609db9ed2c1c1733f977f3959612
carlosjdiez <jdiezcarlos@gmail.com>
2025-08-26 18:37:28 +0200

    inicializa wrapper de Gradle y configuración base del proyecto

---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                // Do not change the username below.
+                // This should always be `mapbox` (not your username).
+                username = "mapbox"
+                // Use the secret token you stored in gradle.properties as the password
+                password =
+                    "sk.eyJ1IjoiY2FybG9zamRpZXoiLCJhIjoiY21lbzk2bm95MDZ1aDJqcXpvemtlZnBidyJ9.ksVHGI_GGu-GhKO1lh9z8g"
+            }
+        }
+    }
+}
+
+rootProject.name = "clinic"
+include(":app")
+ 
